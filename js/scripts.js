ba19aaeb832ce8c6d9202861b12a0d1f25fc980e
bittywahome <bettywahome253@gmail.com>
2019-06-09 14:45:04 +0300

    add footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
 <script>
   $(document).ready(function() {
-        $('div.input-error').hide();
-        $('div.input-success').hide();
-        $('#submitButton').click(function() {
-              if ($('#password_input').val().length > 6) {
-                $('div.input-success').show(); <
+alert($);
+});
+</script>
